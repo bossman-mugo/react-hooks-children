54d5550720f831eb581a7366f8880d08ff6c4d46
Eric <mugoericpg20@gmail.com>
2023-04-22 11:18:35 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ function App() {
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
+
       </Container>
       <Container header="Example header" textPosition="center">
         <img
